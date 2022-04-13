1c35aa57bad8ccdb20ca488335e1185268aa89de
Anna N <info@annadevelops.com>
2022-04-13 13:00:33 +0100

    Install express-async-handler to use async await syntax in express

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,10 @@
+const asyncHandler = require('express-async-handler') //Easier syntax to call async await functions because Moongose will return promises see more - https://github.com/Abazhenov/express-async-handler
+
+
 //@desc    Register a user
 //@route    /api/users
 //@access   Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     const {name, email, password} = req.body
 
     //check if one of those fields are missing then set the res status to 400 then throw a new Error with message 'please include all fields' which will then be caught by the errorMiddleware to send back a json object. If no errorMiddleware configured then the default Express Error Handler will return a HTML page
@@ -11,14 +14,14 @@ const registerUser = (req, res) => {
     }
 
     res.send('User registered')
-}
+})
 
 //@desc    Login a user
 //@route    /api/users/login
 //@access   Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.send('Login Route')
-}
+})
 
 module.exports = {
     registerUser,
