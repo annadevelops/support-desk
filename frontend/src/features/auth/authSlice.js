a12a844094254263b14a68a5be07e53b91956535
Anna N <info@annadevelops.com>
2022-04-14 12:59:23 +0100

    Register user from form to backend

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,7 +1,10 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import authService from './authService'
 
+//Get user from localstorage
+const user = JSON.parse(localStorage.getItem('user'))
 const initialState = {
-    user: null,
+    user: user,
     isError: false,
     isSuccess: false,
     isLoading: false,
@@ -10,7 +13,12 @@ const initialState = {
 
 // Register new user
 export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
-    console.log(user)
+    try {
+        return await authService.register(user)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
 })
 
 // Login user
@@ -21,8 +29,32 @@ export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
 export const authSlice = createSlice({
     name: 'auth',
     initialState,
-    reducers: {},
-    extraReducers: (builder) => {}
+    reducers: {
+        reset: state => {
+            state.isLoading = false
+            state.isError = false
+            state.isSuccess = false
+            state.isLoading = false
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(register.pending, (state) => {
+            state.isLoading = true
+        })
+        .addCase(register.rejected, (state, action) => {
+            state.isLoading = false
+            state.message = action.payload
+            state.user = null
+            state.isError = true
+        })
+        .addCase(register.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isError = false
+            state.user = action.payload
+        })
+    }
 })
 
+export const { reset } = authSlice.actions
 export default authSlice.reducer
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { FaUser } from 'react-icons/fa'
 import { toast } from "react-toastify"
 import { useSelector, useDispatch } from "react-redux"
-import { register } from "../features/auth/authSlice"
+import { register, reset } from "../features/auth/authSlice"
+import { useNavigate } from 'react-router-dom'
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -15,7 +16,19 @@ function Register() {
     const { name, email, password, password2 } = formData
 
     const dispatch = useDispatch()
-    const {user, isLoading, isError, isSuccess} = useSelector(state => state.auth)
+    const {user, isLoading, isError, isSuccess, message} = useSelector(state => state.auth)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if(isError && !isLoading) {
+            toast.error(message)
+         } 
+         
+         if(isSuccess || user ) {
+            dispatch(reset())
+            navigate('/')
+         }
+    }, [isError, isLoading, message, isSuccess, user])
 
     const onChange = (e) => {
         setFormData((prevState) => ({
